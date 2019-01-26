1acfbe0d60c21864876d4bb98f82f27c81372aa0
Ahmed Rabea <Ahmed.rabea.mahmoud@gmail.com>
2019-01-26 03:33:31 +0200

    Validation Alert

--- app.js
+++ app.js
@@ -24,6 +24,31 @@ UI.prototype.addBookToolist = function(book){
 
 }
 
+// Show Alert
+ UI.prototype.showAlert = function(message, className) {
+    // Create div
+    const div = document.createElement('div');
+
+    // Add classes
+    div.className = `alert ${className}`;
+   
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    
+    // Get parent
+    const container = document.querySelector('.container');
+
+    // Get form
+    const form = document.querySelector('#book-form');
+    
+    // Insert alert
+    container.insertBefore(div, form);
+    // Timeout after 3 sec
+    setTimeout(() => {
+        document.querySelector('.alert').remove();
+    }, 3000);
+ }
+
 // Clear Fields
 UI.prototype.clearFields = () =>{
     document.getElementById('title').value = ``;
@@ -44,12 +69,27 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     // Instantiate UI
     const ui = new UI();
 
-    // Add book to list
-    ui.addBookToolist(book);
+    // Validate
+    if(title === '' || author === '' || isbn === '' ){
+        // Error alert
+        ui.showAlert('Please fill in all fields', 'error');
+
+    } else {
+                // Add book to list
+                ui.addBookToolist(book);
+
+                // Show success
+                ui.showAlert('Book Added!', 'success')
     
-    // Clear fields
-    ui.clearFields()
-    
+                // Clear fields
+                ui.clearFields()
+    }
+
+
+        
+
+
+
     
     
     e.preventDefault();
